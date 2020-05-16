63a2b5cb935aad42c2098469b56359047bc5d7eb
= <ductruong1802@gmail.com>
2020-05-16 05:59:50 +0300

    File creation + new structs

--- asm/includes/asm.h
+++ asm/includes/asm.h
@@ -3,5 +3,24 @@
 # define ASM_H
 
 # include "libft.h"
+# include <sys/types.h>
+# include <sys/stat.h>
+# include <fcntl.h>
+
+typedef struct			s_command
+{
+	char				*command;
+	unsigned int		row;
+	struct s_command	*next;
+}						t_command;
+
+typedef struct			s_parser
+{
+	int					fd;
+	unsigned int		row;
+	t_command			*commands;
+	char				*name;
+	char				*comment;
+}						t_parser;
 
 #endif
--- asm/src/asm.c
+++ asm/src/asm.c
@@ -1,14 +1,35 @@
 #include "asm.h"
-#include <errno.h>
+
+void	error_exit(char *error) {
+	ft_printf("%s", error);
+	exit (1);	
+}
+
+char*	change_extension(char *filename, char *extension) {
+	char *filebody;
+	char *s;
+
+	filebody = ft_strndup(filename, ft_strlen(filename) - 2);
+	s = ft_strjoin(filebody, extension);
+	free(filebody);
+	return(s);
+}
 
 void	to_byte_code(char *filename) {
-	return ;
+	int	fd;
+	char *new_fn;
+
+	if ((fd = open(filename, O_RDONLY)) == -1)
+		error_exit("FILE OPEN ERROR");
+	new_fn = change_extension(filename, ".cor");
+	if ((fd = open(new_fn, O_WRONLY | O_APPEND | O_CREAT, 0644)) == -1)
+		error_exit("FILE CREATE ERROR");
+	free(new_fn);
 }
 
 int		check_extension(char *filename, char *extension)
 {
-	int len = ft_strlen(filename);
-	return ft_strstr(filename + (len - ft_strlen(extension)), extension) ? 1 : 0;
+	return ft_strstr(filename + (ft_strlen(filename) - ft_strlen(extension)), extension) ? 1 : 0;
 }
 
 int		main(int argc, char **argv)
